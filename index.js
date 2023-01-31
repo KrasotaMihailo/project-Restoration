dd26584596309c239513982322d8bd63acd6c0c8
KrasotaMihailo <krasotamv@ukr.net>
2023-01-31 21:42:52 +0200

    try-catch

--- index.js
+++ index.js
@@ -1,75 +1,76 @@
-const express = require("express") //подключаем модуль express
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+const express = require('express'); // подключаем модуль express
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const Joi = require('joi');
-const dotenv = require('dotenv').config()
-const swaggerJSDoc = require("swagger-jsdoc")// SWAGGER
-const swaggerUi = require("swagger-ui-express"); // SWAGGER
+const dotenv = require('dotenv').config();
+const swaggerJSDoc = require('swagger-jsdoc');// SWAGGER
+const swaggerUi = require('swagger-ui-express');
+// SWAGGER
+process.on('uncaughtException', () => {
+  console.log('uncaughtException');
+});
 
 const urlencodedParser = bodyParser.urlencoded({ // для передачи параметров через body
-    extended: false,
-})
+  extended: false,
+});
 
 const schemaValid1 = Joi.object({ // схема 1 для валидации с помощью бибилиотеки Joi
-    password: Joi.string().min(4),
-    email: Joi.string()
-        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
-})
+  password: Joi.string().min(4),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+});
 
 const schemaValid2 = Joi.object({ // схема 2 для валидации с помощью бибилиотеки Joi
-    id: Joi.number().min(3),
-    email: Joi.string()
-        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
-})
+  id: Joi.number().min(3),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+});
 
 const schemaValid3 = Joi.object({ // схема 3 для валидации с помощью бибилиотеки Joi
-    id: Joi.number().min(3)
-})
+  id: Joi.number().min(3),
+});
 
 // SWAGGER
 
-const swaggerDefinition = {//общее описание сервера
-    openapi: '3.0.0',
-    info: {
-        title: 'Express API for JSONPlaceholder',
-        version: '1.0.0',
-    },
+const swaggerDefinition = { // общее описание сервера
+  openapi: '3.0.0',
+  info: {
+    title: 'Express API for JSONPlaceholder',
+    version: '1.0.0',
+  },
 };
 
 const options = {
-    swaggerDefinition,
-    // Paths to files containing OpenAPI definitions
-    apis: ['./*.js'], // тут прописывается общий путь к файлам 
+  swaggerDefinition,
+  // Paths to files containing OpenAPI definitions
+  apis: ['./*.js'], // тут прописывается общий путь к файлам
 };
 
 const swaggerSpec = swaggerJSDoc(options);
 
-
 mongoose
-    .connect(process.env.MONGO_URL, { useNewUrlParser: true })
-    .then(() => {
-        const app = express() //вызываем express
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true })
+  .then(() => {
+    const app = express(); // вызываем express
 
-        const SchemaUser = mongoose.Schema({// Схема для формирования базы данных 
-            email: String,
-            password: String,
-            id: Number,
-        });
-        const model = mongoose.model('restoration', SchemaUser); //для связи с MongoDB
-        app.use(urlencodedParser)// подключение bodyParser сразу на все роуты
+    const SchemaUser = mongoose.Schema({ // Схема для формирования базы данных
+      email: String,
+      password: String,
+      id: Number,
+    });
+    const model = mongoose.model('restoration', SchemaUser); // для связи с MongoDB
+    app.use(urlencodedParser);// подключение bodyParser сразу на все роуты
 
+    app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerSpec));// Создаем страницу Swagger
 
-        app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerSpec));//Создаем страницу Swagger
-
-
-        // GET
-        //документация запросов и ответов
-        /**
+    // GET
+    // документация запросов и ответов
+    /**
         * @swagger
         * /users:
         *   get:
         *     summary: получение списка пользователей
-        *     description: работа с учебным сервером,получение списка пользователей        *     
+        *     description: работа с учебным сервером,получение списка пользователей        *
         *     responses:
         *       200:
         *         description: A list of users.
@@ -83,39 +84,37 @@ mongoose
         *                   items:
         *                     type: object
         *                     properties:
-        *                        email: 
+        *                        email:
         *                           type: string
         *                           description: почта пользователя.
         *                           example: 55@ukr.net
-        *                        password: 
+        *                        password:
         *                            type: string
         *                            description: пароль пользователя.
         *                            example: 3333
         *                        id:
         *                         type: integer
         *                         description:  ID пользователя, который запрашивается.
-        *                         example: 123         
+        *                         example: 123
         */
 
-        app.get("/users", async (req, res) => {
-            try {
-                const users = await model.find({});
-                res.send(users)// ответ 
-            }
-            catch (error) { 
-                console.log("catch")
-                res.send("Что-то пошло не так")
-            }
-        })
+    app.get('/users', async (req, res) => {
+      try {
+        const users = await model.find({});
+        res.send(users);// ответ
+      } catch (error) {
+        console.log('catch');
+        res.send('Что-то пошло не так');
+      }
+    });
 
-
-        // POST
-        /**
+    // POST
+    /**
         * @swagger
         * /users:
         *   post:
         *     summary: добавление пользователя
-        *     description: работа с учебным сервером,создание пользователя в базе        
+        *     description: работа с учебным сервером,создание пользователя в базе
         *     parameters:
         *       - in: body
         *         name: email
@@ -124,7 +123,7 @@ mongoose
         *       - in: body
         *         name: password
         *         description: пароль
-        *         type: string 
+        *         type: string
         *     responses:
         *       200:
         *         description: A list of users.
@@ -138,37 +137,43 @@ mongoose
         *                   items:
         *                     type: object
         *                     properties:
-        *                        email: 
+        *                        email:
         *                           type: string
         *                           description: почта пользователя.
         *                           example: 55@ukr.net
-        *                        password: 
+        *                        password:
         *                            type: string
         *                            description: пароль пользователя.
         *                            example: 3333
         *                        id:
         *                         type: integer
         *                         description:  ID пользователя, который запрашивается.
-        *                         example: 123 
+        *                         example: 123
         */
 
-        app.post("/users", async (req, res) => {
-            const { error } = schemaValid1.validate(req.body)// Это блок валидации, валидация идет раньше чем действия по роуту, 
-            if (error) {
-                return res.status(400).json({ message: error.details }); //если валидация не проходит то код дальше не выполняется
-            }
-            const user = model({
-                email: req.body.email,
-                password: req.body.password,
-                id: Math.round(Math.random() * 1000),
-            });
+    app.post('/users', async (req, res) => {
+      const { error } = schemaValid1.validate(req.body);// Это блок валидации, валидация идет раньше чем действия по роуту,
+      if (error) {
+        return res.status(400).json({ message: error.details }); // если валидация не проходит то код дальше не выполняется
+      }
 
-            await user.save()
-            res.send(user)//ответ
-        })
+      try {
+        const user = model({
+          email: req.body.email,
+          password: req.body.password,
+          id: Math.round(Math.random() * 1000),
+        });
 
-        // PATCH
-        /**
+        await user.save();
+        res.send(user);// ответ
+      } catch (error1) {
+        console.log('catch');
+        res.send('Что-то пошло не так');
+      }
+    });
+
+    // PATCH
+    /**
         * @swagger
         * /users:
         *   patch:
@@ -182,7 +187,7 @@ mongoose
         *       - in: body
         *         name: id
         *         description: индивидуальный номер пользователя
-        *         type: number  
+        *         type: number
         *     responses:
         *       200:
         *         description: new user.
@@ -196,35 +201,39 @@ mongoose
         *                   items:
         *                     type: object
         *                     properties:
-        *                        email: 
+        *                        email:
         *                           type: string
         *                           description: новая почта пользователя.
         *                           example: 55@ukr.net
-        *                        password: 
+        *                        password:
         *                           type: string
         *                           description: пароль пользователя.
         *                           example: 3333
         *                        id:
         *                           type: integer
         *                           description:  ID пользователя.
-        *                           example: 123         
+        *                           example: 123
         */
 
-        app.patch("/users", async (req, res) => {
-            const { error } = schemaValid2.validate(req.body)// Это блок валидации
-            if (error) {
-                return res.status(400).json({ message: error.details });
-            }
-            const userPatch = await model.findOne({ id: req.body.id })
-            userPatch.email = req.body.email
-            await userPatch.save()
-            res.send(userPatch)// ответ
-        })
+    app.patch('/users', async (req, res) => {
+      const { error } = schemaValid2.validate(req.body);// Это блок валидации
+      if (error) {
+        return res.status(400).json({ message: error.details });
+      }
+      try {
+        const userPatch = await model.findOne({ id: req.body.id });
+        userPatch.email = req.body.email;
+        await userPatch.save();
+        res.send(userPatch);// ответ
+      } catch (error1) {
+        console.log('catch');
+        res.send('Что-то пошло не так');
+      }
+    });
 
+    // DELETE
 
-        //DELETE
-
-        /**
+    /**
         * @swagger
         * /users:
         *   delete:
@@ -234,7 +243,7 @@ mongoose
         *       - in: body
         *         name: id
         *         description: индивидуальный номер пользователя
-        *         type: number  
+        *         type: number
         *     responses:
         *       200:
         *         description: new user.
@@ -248,33 +257,27 @@ mongoose
         *                   items:
         *                     type: object
         *                     properties:
-        *                        message: 
+        *                        message:
         *                           type: string
         *                           description: "Пользователь удален"
         */
 
-        app.delete("/users", async (req, res) => {
-            const { error } = schemaValid3.validate(req.body)// Это блок валидации
-            if (error) {
-                return res.status(400).json({ message: error.details });
-            }
-            await model.deleteOne({ id: req.body.id })
-            res.send("пользователь удален")// ответ
-        })
+    app.delete('/users', async (req, res) => {
+      const { error } = schemaValid3.validate(req.body);// Это блок валидации
+      if (error) {
+        return res.status(400).json({ message: error.details });
+      }
 
-        app.listen(process.env.PORT, () => {
-            console.log(`Server has started!`);
-        });
+      try {
+        await model.deleteOne({ id: req.body.id });
+        res.send('пользователь удален');// ответ
+      } catch (error1) {
+        console.log('catch');
+        res.send('Что-то пошло не так');
+      }
     });
 
-
-
-
-
-
-
-
-
-
-
-
+    app.listen(process.env.PORT, () => {
+      console.log('Server has started!');
+    });
+  });
